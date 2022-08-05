c34ca1294f3c5e523feb4b88d000e28c6bbc37f3
Madhan Ganesh <madhan.ganesh@angelbroking.com>
2022-08-05 20:35:48 +0530

    added the code to read xls file

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
 	"log"
-	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/shakinm/xlsReader/xls"
 )
 
 func main() {
@@ -18,7 +18,9 @@ func main() {
 	)
 
 	bucket := "mf-core-files/DEV/bank-reverse-files/05082022/"
-	item := "05082022AxisReverseFile.xls"
+	//item := "05082022AxisReverseFile.xls"
+	//item := "05082022SBIReverseFile_286538.xls"
+	item := "286538_04-08-2022_04-08-2022_0408110650 (1).xls"
 
 	downloader := s3manager.NewDownloader(sess)
 	buf := aws.NewWriteAtBuffer([]byte{})
@@ -33,9 +35,34 @@ func main() {
 		log.Fatal(err)
 	}
 
-	strData := string(buf.Bytes())
-	scanner := bufio.NewScanner(strings.NewReader(strData))
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+	//strData := string(buf.Bytes())
+	//fmt.Println(strData)
+
+	reader := bytes.NewReader(buf.Bytes())
+	workbook, err := xls.OpenReader(reader)
+	if err != nil {
+		log.Panic(err.Error())
 	}
+	sheet, err := workbook.GetSheet(0)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	for i := 0; i <= sheet.GetNumberRows(); i++ {
+		if row, err := sheet.GetRow(i); err == nil {
+			if cell, err := row.GetCol(1); err == nil {
+
+				// Значение ячейки, тип строка
+				// Cell value, string type
+				fmt.Println(cell.GetString())
+			}
+		}
+	}
+
+	/*file, err := excelize.OpenReader(strings.NewReader(strData), excelize.Options{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()*/
+
 }
